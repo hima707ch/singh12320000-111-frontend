8fa86c1cd084434ba4445cdd403cdbda76d8e7d9
root <root@feb9c76d30b7>
2025-06-24 11:23:51 +0000

    Frontend setup and components update

---
+++ src/components/Body.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Global/Header.jsx"
+import HomePage
+import logger from "./HomePage/Body.jsx";
+import Login from "./Login/Body.jsx";
+import Register from "./Register/Body.jsx";
+import RoomListing from "./RoomListing/Body.jsx";
+import RoomDetails from "./RoomDetails/Body.jsx";
+
+const Body = () => {
+    return (
+        <Router>
+        <Header />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/roomlisting" element={<RoomListing />} />
+        <Route path="/roomdetails" element={<RoomDetails />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default Body;
